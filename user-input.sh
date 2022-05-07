075efbeba3466e2c553293a072eac93f84aab801
praveen6543 <105046880+praveen6543@users.noreply.github.com>
2022-05-07 18:14:39 +0530

    Created user-input.sh

---
+++ user-input.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter username"
+read username
+
+echo "Enter password"
+read password
+
+if [[ ( $username == "admin" && $password == "pass123" ) ]];
+then
+echo "valid credentials"
+else
+echo "invalid credentials"
+fi
